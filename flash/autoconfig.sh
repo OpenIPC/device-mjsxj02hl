910044ac7226b81ba34fe472695c07cddab13abb
Sergey Avdeev <avdeevsv91@gmail.com>
2022-11-09 15:40:12 +0300

    set osmem

--- flash/autoconfig.sh
+++ flash/autoconfig.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 #
-# Cleaning of the settings partition
+# First boot
 #
+
+# Set osmem (mmz memory = 64 - osmem)
+fw_setenv osmem 35M
+
+# Enable white LED
 $(dirname $0)/autoconfig/usr/sbin/led_control.sh -o 1 -b 1
+
+# Cleaning of the settings partition
 flash_eraseall -j /dev/mtd4
+
+# Reboot
 reboot
