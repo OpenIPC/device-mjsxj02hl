b71f5ee7796f434c08ccf9661f2a262baf287c6b
Sergey Avdeev <avdeevsv91@gmail.com>
2022-11-10 16:56:23 +0300

    Update reset_button.sh

--- flash/autoconfig/usr/sbin/reset_button.sh
+++ flash/autoconfig/usr/sbin/reset_button.sh
@@ -2,6 +2,11 @@
 #
 # Reset button for Xiaomi MJSXJ02HL
 
+if [ ! -d /sys/class/gpio/gpio0/ ]; then
+    echo 0 > /sys/class/gpio/export
+    echo in > /sys/class/gpio/gpio0/direction
+fi
+
 if [ $(cat /sys/class/gpio/gpio0/value) -eq 0 ]; then
     # Wipe overlay partition and reboot
     echo "Let's start wipe overlay partition (/dev/mtd4)"
