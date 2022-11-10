9a6f3ff7672cd9656324eb53679318dae1a85d84
avdeevsv91 <avdeevsv91@gmail.com>
2022-11-10 09:30:51 +0300

    sysupgrade instead of flash_eraseall

--- flash/autoconfig.sh
+++ flash/autoconfig.sh
@@ -10,8 +10,5 @@ fw_setenv osmem 35M
 # Enable white LED
 $(dirname $0)/autoconfig/usr/sbin/led_control.sh -o 1 -b 1
 
-# Cleaning of the settings partition
-flash_eraseall -j /dev/mtd4
-
-# Reboot
-reboot
+# Wipe overlay partition and reboot
+/usr/sbin/sysupgrade -n
