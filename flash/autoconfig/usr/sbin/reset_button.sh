173dfc3f5096317414535f305dc986cfe15d5380
avdeevsv91 <avdeevsv91@gmail.com>
2022-11-10 11:17:48 +0300

    sysupgrade instead of flash_eraseall

--- flash/autoconfig/usr/sbin/reset_button.sh
+++ flash/autoconfig/usr/sbin/reset_button.sh
@@ -3,11 +3,7 @@
 # Reset button for Xiaomi MJSXJ02HL
 
 if [ $(cat /sys/class/gpio/gpio0/value) -eq 0 ]; then
-    /etc/init.d/S96autonight stop
-    /etc/init.d/S00autoled stop
-    killall majestic
-    /usr/sbin/led_control.sh -o 1 -b 1
-    echo "Let's start erasing the rootfs_data (/dev/mtd4) partition"
-    flash_eraseall -j /dev/mtd4
-    reboot
+    # Wipe overlay partition and reboot
+    echo "Let's start wipe overlay partition (/dev/mtd4)"
+    /usr/sbin/sysupgrade -n -z
 fi
